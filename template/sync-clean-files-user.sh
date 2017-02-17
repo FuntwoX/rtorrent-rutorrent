d18dacd7560f7a76e47e6efc183c8b0137cd2a46
FuntwoX <FuntwoX@users.noreply.github.com>
2017-02-17 19:22:13 +0100

    Revert "Correction on rsync"
    This reverts commit 1006a62fa0248e5f59e85e5578cb276307fd5e92.

--- template/sync-clean-files-user.sh
+++ template/sync-clean-files-user.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rsync -a /downloads/rtorrent/#user#/plex/movies/ /downloads/rtorrent/#user#/postprocessing/couchpotato
-rsync -a /downloads/rtorrent/#user#/plex/series/ /downloads/rtorrent/#user#/postprocessing/sickrage
+rsync -a /downloads/rtorrent/#user#/plex/movies /downloads/rtorrent/#user#/postprocessing/couchpotato/
+rsync -a /downloads/rtorrent/#user#/plex/series /downloads/rtorrent/#user#/postprocessing/sickrage/
 
 find /downloads/rtorrent/#user#/postprocessing/ -mindepth 1 -mtime +2 -delete
